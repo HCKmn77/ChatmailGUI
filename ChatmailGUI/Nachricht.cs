83b9b8bafd2a899ee0346289b109bb2c4f7b6f7e
Jeremia Haackmann <anmax99@web.de>
2020-05-07 14:59:41 +0200

    Nachrichten Klasse angelegt

--- ChatmailGUI/Nachricht.cs
+++ ChatmailGUI/Nachricht.cs
@@ -8,6 +8,32 @@ namespace ChatmailGUI
 {
     class Nachricht
     {
+        string nachrichtenText;
+        Benutzer nachrichtenSender;
+        int NachrichtenID;
+
+        public Nachricht(string text, Benutzer sender, int id)
+        {
+            nachrichtenText = text;
+            nachrichtenSender = sender;
+            NachrichtenID = id;
+        }
+
+
+        public string text()
+        {
+            return nachrichtenText;
+        }
+
+        public Benutzer sender()
+        {
+            return nachrichtenSender;
+        }
+
+        public int id()
+        {
+            return NachrichtenID;
+        }
 
 
     }
